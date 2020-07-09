b80c694265cd4d867409993054efa1a1a3553476
josemamunoz <josema.munoz@gmail.com>
2020-07-08 22:23:56 -0400

    agregue welcome

---
+++ src/js/views/welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Login } from "../component/login"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+export const Welcome = () => (
+	<div className="container">
+		<Login/>
+	</div>
+);
