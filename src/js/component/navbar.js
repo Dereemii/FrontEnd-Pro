ec97c641d5942ac7a2c5ce868f22a64559e44808
Dereemii <leshermira@gmail.com>
2020-07-18 09:18:44 -0400

    navbar display

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import iconodetareas from "../component/img/iconodetareas.png"
 import iconoTrofeo from "../component/img/iconoTrofeo.png"
 import iconoEngranaje from "../component/img/iconoEngranaje.png"
+import iconoAvatar from "../component/img/iconoAvatar.png"
+import iconoHome from "../component/img/iconoHome.png"
+
 
 export const Navbar = () => {
 
@@ -9,27 +12,29 @@ export const Navbar = () => {
 		<>
 			<div className="container-fluid">
 				<div>
-					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					<nav className="navbar navbar-expand-lg navbar-light bg-info border border-light">
 						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 							<span className="navbar-toggler-icon"></span>
 						</button>
 						<div className="collapse navbar-collapse" id="navbarNav">
-							<ul className="navbar-nav">
-								<li className="nav-item active">
-									<a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+							<ul className="navbar-nav text-center">
+								<li className="col-2"></li>
+								<li className="col-2 mx-3">
+									<a className="nav-link" href="/"><img src={iconoAvatar} width="80px"/> </a>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/">Features</a>
+								<li className="col-2 mx-3">
+									<a className="nav-link" href="/"><img src={iconoHome} width="80px"/></a>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/"> <img src={iconodetareas} width="30px"/> </a>
+								<li className="col-2 mx-3">
+									<a className="nav-link" href="/"> <img src={iconodetareas} width="80px"/> </a>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/"> <img src={iconoTrofeo} width="30px"/></a>
+								<li className="col-2 mx-3">
+									<a className="nav-link" href="/"> <img src={iconoTrofeo} width="80px"/></a>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/"> <img src={iconoEngranaje} width="30px"/></a>
+								<li className="col-2 mx-3">
+									<a className="nav-link" href="/"> <img src={iconoEngranaje} width="80px"/></a>
 								</li>
+								<li className=""></li>
 							</ul>
 						</div>
 					</nav>
