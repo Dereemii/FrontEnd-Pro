8b89f02bb10a0257592612d9030504410342671d
Dereemii <leshermira@gmail.com>
2020-07-18 23:01:45 -0400

    actualizacion ruleta

--- src/js/views/juegos/ruleta.jsx
+++ src/js/views/juegos/ruleta.jsx
@@ -3,102 +3,106 @@ import './ruleta.css';
 
 
 export const Ruleta = () => (
-   
-    <>
     
-    <div className="container pt-5 mt-5">
-        <div className="arrow "></div>
-        <ul className="circle">
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    1
+    <>
+        <div className="text-center mt-5">
+            <h1>
+                Presiona el boton para una <br/> pregunta aleatoria
+            </h1>
+        </div>
+        <div className="container-fluid pt-5 ">
+            <div className="arrow "></div>
+            <ul className="circle">
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        1
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    2
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        2
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    3
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        3
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    4
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        4
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    5
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        5
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    6
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        6
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    7
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        7
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    8
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        8
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    9
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        9
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    10
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        10
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    11
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        11
                 </div>
-            </li>
-            <li>
-                <div className="text"
-                    contentEditable="true"
-                    spellCheck="false">
-                    12
+                </li>
+                <li>
+                    <div className="text"
+                        contentEditable="true"
+                        spellCheck="false">
+                        12
                 </div>
-            </li>
-        </ul>
-    </div>
+                </li>
+            </ul>
+        </div>
 
-   <div className="mx-auto py-5 ">
-   <button type="button" className="btn btn-success btn-lg mx-auto px-5 py-3" >GIRAR</button>
-   </div>
-</>
+        <div className="mx-auto py-5 ">
+            <button type="button" className="btn btn-success btn-lg mx-auto px-5 py-3" >GIRAR</button>
+        </div>
+    </>
 
 );
