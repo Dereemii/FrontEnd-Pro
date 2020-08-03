cd5f962685a4d97ba6a9ec666fa9f0989d937f42
Dereemii <leshermira@gmail.com>
2020-08-03 12:50:23 -0400

    fontawesome library

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons'
+import './navbar.css';
 
 
 export const Navbar = () => {
+
+	const menuIcon = <FontAwesomeIcon icon={faChevronCircleDown} />
 
 	return (
 		<>
 			<div className="container-fluid">
 				<div>
 					<nav className="navbar navbar-expand-lg navbar-light bg-info border border-light">
-						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
+						<button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+							aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="nav-icon" title="MENU" >{menuIcon}</span>
 						</button>
 						<div className="collapse navbar-collapse" id="navbarNav">
 							<div class="row navbar-nav mx-auto">
@@ -18,7 +24,7 @@ export const Navbar = () => {
 									</a>
 								</div>
 								<div class="col-sm px-5 ">
-									<a className="nav-link" href="/"><img className="img-fluid"  title="INICIO" src="../../img/iconoHome.png" width="100px" /></a>
+									<a className="nav-link" href="/"><img className="img-fluid" title="INICIO" src="../../img/iconoHome.png" width="100px" /></a>
 								</div>
 								<div class="col-sm px-5">
 									<a className="nav-link" href="/"> <img className="img-fluid" title="TAREAS" src="../../img/iconodetareas.png" width="100px" /> </a>
@@ -27,7 +33,7 @@ export const Navbar = () => {
 									<a className="nav-link" href="/"> <img className="img-fluid" title="RANKING" src="../../img/iconoTrofeo.png" width="100px" /></a>
 								</div>
 								<div class="col-sm px-5">
-									<a className="nav-link" href="/"> <img className="img-fluid" title="CONFIGURACION "src="../../img/iconoEngranaje.png" width="100px" /></a>
+									<a className="nav-link" href="/"> <img className="img-fluid" title="CONFIGURACION " src="../../img/iconoEngranaje.png" width="100px" /></a>
 								</div>
 							</div>
 						</div>
