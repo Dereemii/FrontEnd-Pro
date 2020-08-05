2aeda34c5d267f2a4e0b09b81d5e321fd8dcdab9
Dereemii <leshermira@gmail.com>
2020-08-05 16:40:05 -0400

    paso al español clases css y ruleta

--- src/js/views/juegos/ruleta.jsx
+++ src/js/views/juegos/ruleta.jsx
@@ -5,16 +5,16 @@ import './ruleta.css';
 export class Ruleta extends React.Component {
 
     state = {
-        name: "circle" // estilo circle
+        name: "circulo" // estilo circulo
     }
 
-    startRotation = () => {
+    startRotation = () => {  {/* Funcion que da inicio a la rotacion */}
         this.setState({
-            name: "circle start-rotate" // estilo + css animacion
+            name: "circulo iniciar-rotacion" // estilo + css animacion
         });
         setTimeout(() => {
             this.setState({
-                name: "circle start-rotate stop-rotate"
+                name: "circulo iniciar-rotacion detener-rotacion"
             })
         }, Math.floor(Math.random() * 10000) + 1); //selecion aleatoria de numero
     }
@@ -26,91 +26,91 @@ export class Ruleta extends React.Component {
             <>
                 <div className="text-center mt-5">
                     <h1>
-                        Presiona el boton para una <br /> {/* pregunta aleatoria */}
+                        Presiona el boton para una pregunta aleatoria <br /> {/* pregunta aleatoria */}
                     </h1>
                 </div>
                 <div className="container-fluid pt-5 ">
-                    <div className="arrow "></div>
-                    <ul className={this.state.name}> {/*  paso estilo css de circle */}
+                    <div className="flecha-ruleta"></div>
+                    <ul className={this.state.name}> {/*  paso estilo css de circulo */}
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 1
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 2
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 3
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 4
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 5
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 6
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 7
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 8
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 9
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 10
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 11
             </div>
                         </li>
                         <li class="li-ruleta">
-                            <div className="text"
+                            <div className="texto-ruleta"
                                 contentEditable="true"
                                 spellCheck="false">
                                 12
