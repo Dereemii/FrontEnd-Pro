b3b0fea6626877ba9d9ecc0b1cc615cdc0bed433
Dereemii <leshermira@gmail.com>
2020-09-06 19:00:59 -0300

    rutas navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link, withRouter, useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import { Context } from "../store/appContext";
@@ -12,29 +12,33 @@ export const Navbar = () => {
 	const history = useHistory();
 	const IconoMenu = <FontAwesomeIcon icon={faBars} />
 	const { currentUser } = store;
-	const avatar = !!currentUser && currentUser !== "invitado" ? actions.obtenerAvatar(currentUser.usuario.avatar) : "../../img/iconoAvatar.png";
+	const avatar = !!currentUser && currentUser !== "invitado" ? actions.obtenerAvatar(currentUser.usuario.avatar) : "../../img/avatar.JPG";
 	const name = !!currentUser ? currentUser.usuario.nombre_usuario : "Invitado";
+
 
 	return (
 		<>
 			<div className="container-fluid p-0 m-0">
 				<div>
-					<nav className="navbar navbar-expand-lg justify-content-center">
+					<nav className="navbar navbar-expand-sm navbar-light justify-content-center">
 						<div className="row justify-content-center">
-								<Link className="navbar-brand text-light" to="/">TataExpert</Link>
+								<Link className="navbar-brand text-light" to="/seleccion_curso">TataExpert</Link>
 								<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 									<span className="iconoNav">{IconoMenu}</span>
 								</button>
 								<div className="collapse navbar-collapse" id="navbarNav">
-									<ul className="navbar-nav ml-5">
-										<li className="nav-item active">
-											<Link className="nav-link text-light " to="/">Puntos <span className="sr-only">(current)</span></Link>
+									<ul className="navbar-nav ml-5 text-center">
+										<li className="nav-item">
+											<Link className="nav-link text-light " to="/seleccion_curso">Cursos <span className="sr-only">(current)</span></Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link text-light" to="/">Ajustes</Link>
+											<Link className="nav-link text-light " to="/seleccion_curso">Puntos <span className="sr-only">(current)</span></Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link text-light" to="/">Adminisitrador</Link>
+											<Link className="nav-link text-light text-center" to="/seleccion_curso">Ajustes</Link>
+										</li>
+										<li className="nav-item text-center">
+											<Link className="nav-link text-light" to="/administrador">Adminisitrador</Link>
 										</li>
 										<li className="nav-item">
 											<Link className="nav-link text-light" to={"/editar_usuario"}>{name}</Link>
@@ -84,7 +88,6 @@ export const Navbar = () => {
 										<img className="icon-img" title="Mi perfil" data-toggle="tooltip" data-placement="bottom" src={avatar} />
 									</Link>
 								</div>
-
 								<div className="col-sm px-5 ">
 									<Link to="/seleccion_curso" className="nav-link" href="/">
 										<img className="icon-img" title="Inicio" data-toggle="tooltip" data-placement="bottom" src="../../img/iconoHome.png" />
