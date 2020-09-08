e0da9d80f7cbfa18517ed22d406187aaeaf6cbe2
Dereemii <leshermira@gmail.com>
2020-09-08 19:26:42 -0300

    teoria

---
+++ src/js/views/Lecciones/itemTeoria.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const itemTeoria = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+itemTeoria.propTypes = {
+
+}
+
+export default itemTeoria
